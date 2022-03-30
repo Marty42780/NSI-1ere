e543199ebdaf0349f3fad6f5465f0649d816db4b
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2022-03-30 09:50:10 +0200

    changes of 30/03

--- 01-Html_Css_Eleves/js/navigation.js
+++ 01-Html_Css_Eleves/js/navigation.js
@@ -1,74 +1,56 @@
-acceuil = ["#", "disable"]
-theme1 = ["themes/theme1.html", "enable"]
-theme2 = ["themes/theme2.html", "enable"]
-theme3 = ["themes/theme3.html", "enable"]
-theme4 = ["themes/https://youtu.be/dQw4w9WgXcQ", "enable"]
-theme5 = ["themes/animaux.html", "enable"]
-theme6 = ["themes/tierlist.html", "enable"]
+try {if (!acceuil[1] === "disable") {acceuil = ["index.html", "enable"]}} catch (e) {acceuil = ["index.html", "enable"]}
+try {if (!themes[1] === "disable") {themes = ["themes.html", "enable"]}} catch (e) {themes = ["themes.html", "enable"]}
+try {if (!animaux[1] === "disable") {animaux = ["animaux.html", "enable"]}} catch (e) {animaux = ["animaux.html", "enable"]}
+try {if (!tierlist[1] === "disable") {tierlist = ["tierlist.html", "enable"]}} catch (e) {tierlist = ["tierlist.html", "enable"]}
 
 document.write(`
+<link rel="stylesheet" href="css/navigation.css" />
 <section id="top-bar">
     <h1>NSI - Acceuil</h1>
     <ul id="menu">
-        <a href="` + acceuil[0] + `" target="_parent" class="` + acceuil[1] + ` zero">
+        <a href="` + acceuil[0] + `" class="` + acceuil[1] + ` accueil">
             <li>Accueil</li>
         </a>
-        <a href="` + theme1[0] + `" class="` + theme1[1] + ` one">
-            <li>Theme 1</li>
+        <a href="` + themes[0] + `" class="` + themes[1] + ` themes">
+            <li>Thèmes</li>
         </a>
-        <a href="` + theme2[0] + `" class="` + theme2[1] + ` two"> 
-            <li>Theme 2</li> 
+        <a href="` + animaux[0] + `" class="` + animaux[1] + ` animaux"> 
+            <li>Animaux</li> 
         </a>
-        <a href="` + theme3[0] + `" class="` + theme3[1] + ` three">
-            <li>Theme 3</li>
-        </a>
-        <a href="` + theme4[0] + `" class="` + theme4[1] + ` four">
-            <li>Theme 4</li>
-        </a>
-        <a href="` + theme5[0] + `" class="` + theme5[1] + ` five">
-            <li>Theme 5</li>
-        </a>
-        <a href="` + theme6[0] + `" class="` + theme6[1] + ` six">
-            <li>Theme 6</li>
+        <a href="` + tierlist[0] + `" class="` + tierlist[1] + ` tierlist"> 
+            <li>Tierlist</li> 
         </a>
         <button id="menuButton" onclick="showMenu()">Tout voir</button>
     </ul>
 </section>
 <div id="menuDiv">
 <div class="grid">
-    <a href="themes/theme1.html">
-        <div class="theme1">
+    <a href="` + acceuil[0] + `">
+        <div class="` + acceuil[1] + ` accueil">
             <img src="images/web-dev.png" alt="" />
-            <h2>Thème 1</h2>
-            <h1>Développement Web</h1>bju,
+            <h2>Acceuil</h2>
+            <h1>Introduction</h1>
         </div>
     </a>
-    <a href="themes/theme2.html">
-        <div class="theme2">
+    <a href="` + themes[0] + `">
+        <div class="` + themes[1] + ` themes">
             <img src="images/binaire.png" alt="" />
-            <h2>Thème 2</h2>
-            <h1>Binaire et Hexadécimal</h1>
+            <h2>Thèmes</h2>
+            <h1>Thèmes de NSI</h1>
         </div>
     </a>
-    <a href="themes/theme3.html">
-        <div class="theme3">
+    <a href="` + animaux[0] + `">
+        <div class="` + animaux[1] + ` animaux">
             <img src="images/algobox-dev.png" alt="" />
-            <h2>Thème 3</h2>
-            <h1>Algorithmique alogobox</h1>
+            <h2>Animaux</h2>
+            <h1>Promenade avec les animaux</h1>
         </div>
     </a>
     <a href="https://youtu.be/dQw4w9WgXcQ">
-        <div class="theme4">
+        <div class="` + animaux[1] + ` animaux"">
             <img src="images/python-logo.jpg" alt="" />
-            <h2>Thème 4</h2>
-            <h1>Introduction Python</h1>
-        </div>
-    </a>
-    <a href="themes/animaux.html">
-        <div class="theme5">
-            <img src="images/lezard.jpg" alt="">
-            <h2>Thème 5</h2>
-            <h1>Promenade avec les animaux</h1>
+            <h2>Youtube</h2>
+            <h1>Never Gonna Give You Up</h1>
         </div>
     </a>
 </div>
